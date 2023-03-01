60b531b247334cc09aaa440393f377fd0707b27e
yehonatanyosefi <123459744+yehonatanyosefi@users.noreply.github.com>
2023-03-01 20:44:07 +0200

    styled buttons

--- apps/keep/cmps/NoteTodos.js
+++ apps/keep/cmps/NoteTodos.js
@@ -2,8 +2,8 @@ export default {
      props: ['note'],
      template: `
           <ul class="clean-list">
-               <li v-for="todo,idx in note.info.todos" key="idx">
-                         <textarea v-model="todo.txt" @input="updateNote"></textarea>
+               <li v-for="todo,idx in note.info.todos" key="idx" class="flex">
+                         <input v-model="todo.txt" @input="updateNote" placeholder="text"></input>
                          <button @click="deleteTodo(idx)">X</button>
                </li>
                <li><button @click="addTodo">add</button></li>
